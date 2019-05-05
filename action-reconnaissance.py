533dfcd589ec615665a9709c1b3125a80051909a
XRobquin <xrobquin@enseirb-matmeca.fr>
2019-05-05 13:47:02 +0200

    Update action-reconnaissance.py

--- action-reconnaissance.py
+++ action-reconnaissance.py
@@ -8,35 +8,6 @@ MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 
-def verbalise_hour(i):
-	if i == 0:
-		return "minuit"
-	elif i == 1:
-		return "une heure"
-	elif i == 12:
-		return "midi"
-	elif i == 21:
-		return "vingt et une heures"
-	else:
-		return "{0} heures".format(str(i)) 
-
-def verbalise_minute(i):
-	if i == 0:
-		return ""
-	elif i == 1:
-		return "une"
-	elif i == 21:
-		return "vingt et une"
-	elif i == 31:
-		return "trente et une"
-	elif i == 41:
-		return "quarante et une"
-	elif i == 51:
-		return "cinquante et une"
-	else:
-		return "{0}".format(str(i)) 
-
-
 def intent_received(hermes, intent_message):
 
 	print()
@@ -44,15 +15,14 @@ def intent_received(hermes, intent_message):
 	print ()
 
 	if intent_message.intent.intent_name == 'xrobquin:Reconnaissance_proche':
+		
+		if len(intent_message.slots) == 1
 
-		sentence = 'Il est '
-		print(intent_message.intent.intent_name)
-
-		now = datetime.now(timezone('Europe/Paris'))
-
-		sentence += verbalise_hour(now.hour) + " " + verbalise_minute(now.minute)
-		print(sentence)
-		hermes.publish_end_session(intent_message.session_id, sentence)
+			sentence = 'Salut  '		
+			name = intent_message.slots.forecast_location.first()
+			sentence += name 
+			print(sentence)
+			hermes.publish_end_session(intent_message.session_id, sentence)
 
 
 with Hermes(MQTT_ADDR) as h:
