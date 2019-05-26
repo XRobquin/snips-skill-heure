a7c746d9f9c81793ebb132feb052fe5613887dfd
XRobquin <xrobquin@enseirb-matmeca.fr>
2019-05-26 18:38:27 +0200

    Update action-reconnaissance.py

--- action-reconnaissance.py
+++ action-reconnaissance.py
@@ -19,41 +19,37 @@ def intent_received(hermes, intent_message):
 	now = datetime.datetime.now()
 	year = now.year
 	today = datetime.date.today()
-	anniv = datetime.date(year+1, manniv, janniv) 
+	anniv = datetime.date(year+1, 6, 3) 
 	diff = anniv - today
 
 
 
 	if intent_message.intent.intent_name == 'xrobquin:Reconnaissance_proche':
 		
-		if (diff.days%365-1) == 0:
-    			liste_reponses_marie = ["Je vous souhaite un très heureux anniversaire"]
-		else:
-    			liste_reponses_marie = ["j'adore votre haut",
-                              "Comment allez vous aujourd'hui?",
-                              "J moins "+str(diff.days%365-1)+" avant votre anniversaire",
-                              "Je suis ravie de faire la conversation avec vous",
-                              "On commande des sushis ?",
-                              "J'ai hâte de voir monsieur Robquin habillé en requin",
-                              "Etes-vous allée courir aujourd'hui ?",
-                              "Puis-je vous demander une carte postale de Namur ?" ]
-			
+		liste_reponses_appetit = [" J'adore ce haut", " Comment allez vous aujourd'hui?", " j'ai besoin de repos"]
 		sentence = 'Salut '	
 		
 		if len(intent_message.slots.Name)==1:
 			name = intent_message.slots.Name.first().value
 			if (name =='William'):
 				name = 'Williame'
-				result_sentence = 'Ravie de discuter avec vous'
-			elif (name =='Marie'):
-				index_reponse = random.randint(0,len(liste_reponses_marie)-1)
-				result_sentence = liste_reponses_marie[index_reponse]
 			    
 			sentence += name
 			sentence += ', '
-			sentence += result_sentence
+			sentence += liste_reponses_appetit[random.randint(0,len(liste_reponses_appetit)-1)]
+			sentence += str(diff.days%365-1)
 			
-				
+			
+			    
+			
+			
+		if len(intent_message.slots.Name2)==1:
+			sentence += ' et salut '
+			name = intent_message.slots.Name2.first().value
+			if (name =='William'):
+				name = 'Williame'
+
+			
 			
 				
 			
@@ -61,12 +57,4 @@ def intent_received(hermes, intent_message):
 
 
 with Hermes(MQTT_ADDR) as h:
-	h.subscribe_intents(intent_received).start()
-	
-	
-	
-	
-	
-	
-	
-	
+h.subscribe_intents(intent_received).start()
